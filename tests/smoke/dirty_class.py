9d200bd6c76c7c5f8258dab2c63b8e16d7a9dea7
Ivan Ogasawara <ivan.ogasawara@gmail.com>
2026-03-03 22:01:09 +0000

    feat: Add support for classes and methods properly

---
+++ tests/smoke/dirty_class.py
@@ -0,0 +1,13 @@
+class MyClass:
+    """
+    title: My Class
+    """
+
+    def __init__(self, count: int, extra: str):
+        self.count = count
+
+    def do_something(self, val: float) -> bool:
+        """
+        title: Do something
+        """
+        return True
